0d710480bfc14b3cdb327aeef5f6654bdd6fba61
Mohammed Osman <muhammedosmanraheemkhan@gmail.com>
2025-09-11 15:15:35 +0530

    feat : Preloading page intitalised and nheader started

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Preloader from "../components/preloader/preloader";
 import "./globals.css";
+import Header from "../components/header/page";
 
 const page = () => {
   return (
     <>
       <Preloader />
+      <Header />
       <div className="orbitron">Helloooo SMC!!!!</div>
       <div>Helloooo SMC!!!!</div>
     </>
--- src/components/header/page.tsx
+++ src/components/header/page.tsx
@@ -3,7 +3,11 @@ import React from 'react'
 const page = () => {
   return (
     <>
-    <div></div>
+    <div>
+      <div id='logo' className='flex items-center justify-center 50px'>
+        <img src='/SMCMJ.png' alt='logo' className='' />
+      </div>
+    </div>
     </>
   )
 }
